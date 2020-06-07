808e23a02824ee7d10685ac0045a5479a27d6df0
Dolphine Atieno <awuordolphine65@gmail.com>
2020-06-07 07:15:47 +0300

    add model profile

--- projectapp/models.py
+++ projectapp/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch  import receiver
 from django.http import Http404
+from django.utils import timezone
 # Create your models here.
 
 
@@ -40,3 +41,25 @@ class Projects(models.Model):
         except ObjectDoesNotExist:
             raise Http404()
         return Project
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.png', upload_to='profile_photos/')
+    bio = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.user.username}Profile'
+
+    class Meta:
+        db_table = 'profile'
+
+    @receiver(post_save, sender=User)
+    def update_create_profile(sender,instance,created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
+
+
+    def save_profile(self):
+        self.save()

40d0cfe442e82cb88903bd70b85bc261cb4e61d3
Dolphine Atieno <awuordolphine65@gmail.com>
2020-06-07 13:53:59 +0300

    defined user authentication in views

--- projectapp/views.py
+++ projectapp/views.py
@@ -97,3 +97,18 @@ def search_projects(request):
             "message":message,
         }
         return render(request, 'search.html', context)
+
+
+def registration(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            form.save()
+            return redirect('/login')
+    else:
+        form = RegisterForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'users/register.html', context)

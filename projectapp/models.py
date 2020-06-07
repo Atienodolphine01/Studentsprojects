6cfd9fb358a578f37c2f6d5b8f4d3bca579fe5ba
Dolphine Atieno <awuordolphine65@gmail.com>
2020-06-07 06:23:15 +0300

    add imports to the models

--- projectapp/models.py
+++ projectapp/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch  import receiver
+from django.http import Http404
 # Create your models here.
 
 

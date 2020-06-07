94d39bda010d68b89d58515566d59dbc77c4da89
Dolphine Atieno <awuordolphine65@gmail.com>
2020-06-07 19:54:25 +0300

    installed django ratings

--- studeproject/settings.py
+++ studeproject/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'bootstrap4',
     'rest_framework',
     'crispy_forms',
+    'star_ratings',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

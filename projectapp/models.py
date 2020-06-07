d5452d614510c03d41d28b32e5bde225865de4ee
Dolphine Atieno <awuordolphine65@gmail.com>
2020-06-07 06:20:00 +0300

    add functionalities to model projects

--- projectapp/models.py
+++ projectapp/models.py
@@ -11,3 +11,29 @@ class Projects(models.Model):
     title = models.CharField(max_length=255)
     link = models.URLField()
     author_profile = models.ForeignKey(Profile, on_delete=models.CASCADE,default='1', blank=True)
+
+    def save_project(self):
+        self.save()
+
+    def __str__(self):
+        return f'{self.author}Post'
+
+    class Meta:
+        db_table = 'project'
+        ordering = [-created_date]
+
+    def delete_project(self):
+        self.delete()
+
+    @classmethod
+    def search_projects(cls,search_term):
+        project = cls.objects.filter(title__icontains=search_term)
+        return project
+
+    @classmethod
+    def get_object(cls,id):
+        try:
+            project = Projects.objects.get(pk=id)
+        except ObjectDoesNotExist:
+            raise Http404()
+        return Project

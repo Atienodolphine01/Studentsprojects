22eb04ef21a33a4ede87c5bf9c8d42a5ee04b04b
Dolphine Atieno <awuordolphine65@gmail.com>
2020-06-08 16:22:55 +0300

    edit viewS

--- projectapp/views.py
+++ projectapp/views.py
@@ -27,7 +27,7 @@ def updateprofile(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            messages.success(request, f'Your account has been successfully updated')
+            messages.success(request, f'Your account has been updated successfully')
             return redirect('profile')
     else:
         user_form = UserUpdateForm(instance=request.user)
@@ -53,7 +53,7 @@ def profile(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            messages.success(request, f'Your account has been successfully updated')
+            messages.success(request, f'Your account has been updated successfully')
             return redirect('profile')
     else:
         user_form = UserUpdateForm(instance=request.user)
